0f9cc43d678a6055684d52597a1194544b9afe81
lemon-battery <0_david11_!1@proton.me>
2026-04-11 10:13:51 +0100

    fix linting (forgot to add eslint.config.js, oops!)

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+const { defineConfig } = require("eslint/config");
+const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = defineConfig([
+   {
+      files: ["**/*.js", "**/*.ts"],
+      languageOptions: {
+         parser: tsParser,
+         parserOptions: { project: "tsconfig.json", ecmaVersion: "latest", sourceType: "module" }
+      },
+      plugins: { js: js, "@typescript-eslint": tsPlugin },
+      extends: ["js/recommended", "@typescript-eslint/recommended"]
+   },
+   {
+      rules: {
+         "no-unused-vars": "warn",
+         "no-undef": "warn"
+      }
+   }
+]);
