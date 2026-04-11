83bbd6dfd2c28ddac9281d7a62ba9718fb3fd7b6
lemon-battery <0_david11_!1@proton.me>
2026-04-11 17:42:47 +0100

    oops, can't use env in flat config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,15 +2,18 @@ const { defineConfig } = require("eslint/config");
 const js = require("@eslint/js");
 const tsPlugin = require("@typescript-eslint/eslint-plugin");
 const tsParser = require("@typescript-eslint/parser");
+const globals = require("globals");
 
 module.exports = defineConfig([
    {
       files: ["**/*.js", "**/*.ts"],
       languageOptions: {
          parser: tsParser,
-         parserOptions: { project: "tsconfig.json", ecmaVersion: "latest", sourceType: "module" }
+         parserOptions: { project: "tsconfig.json", ecmaVersion: "latest", sourceType: "module" },
+         globals: {
+            ...globals.browser
+         }
       },
-      env: { "browser": true },
       plugins: { js: js, "@typescript-eslint": tsPlugin },
       extends: ["js/recommended", "@typescript-eslint/recommended"]
    },
